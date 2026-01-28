bcbed86874b280bafd1055ee5532e44570c3f29c
Eudora-Yudee <eudoraamara@gmail.com>
2026-01-28 01:04:20 +0100

    push financial health codes for budget-architecture

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter Your Name: ");
+let weeklyBudget = parseInt(prompt("Enter Weekly Budget: "));
+
+const calculateDaily = (total) => {
+    return total/7;
+}
+
+let dailyLimit = calculateDaily(weeklyBudget);
+
+if (dailyLimit < 2000) {
+  console.log ("Avoid luxury expenses");
+} 
+else if (dailyLimit >= 2000 && dailyLimit <= 10000) {
+  console.log ("You have a balanced budget");
+} 
+else {
+  console.log("Consider investing your surplus");
+}
